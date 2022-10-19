23e3254aed19dc0d1bcfcd4497a039dbeca11c8b
Stevemacha123 <116178256+Stevemacha123@users.noreply.github.com>
2022-10-19 16:31:52 +0300

    Add files via upload

---
+++ formhandler.php
@@ -0,0 +1,28 @@
+<?php
+$name = $-POST['name'];
+$results = $-POST['result'];
+$gender = $-POST['gender'];
+$email = $-POST['email'];
+$phone = $-POST['phone'];
+
+$emailfrom = 'Thika High School';
+
+$emailsubject = 'New Student Application' ;
+
+$emailbody = "User Name: $name.\n"
+"User Results: $results.\n"
+"User Gender: $gender.\n"
+"User Email: $email.\n"
+"User Phone: $phone.\n";
+
+$to = 'machasteve1@gmail.com';
+
+$headers = "From: $emailfrom \r\n";
+
+$headers = "Reply-To: $email \r\n";
+
+mail($to,$emailsubject,$emailbody,$headers);
+
+header("Location: logic page.html");
+
+?>
